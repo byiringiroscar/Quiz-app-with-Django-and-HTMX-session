7fef49d6bff5e10b96d019a8d8a5451e6cc2f75a
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-03-25 15:13:43 +0200

    Update requirements and admin

--- sim/admin.py
+++ sim/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Quiz, Question, Answer
 
 # Register your models here.
+
+
+admin.site.register(Quiz)
+admin.site.register(Question)
+admin.site.register(Answer)
+
